a14cea1da3abebccc71fe0e647464a0062b066cc
precioustosin <precioustosin@hotmail.com>
2019-04-08 11:10:34 +0100

    made adjustments to user tests

--- test/user.js
+++ test/user.js
@@ -1,11 +1,11 @@
-/* globals describe, it, beforeEach */
+/* globals describe, it */
 process.env.NODE_ENV = 'test';
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
-const { testDelete } = require('../controllers/user');
+// const { testDelete } = require('../controllers/user');
 
 chai.should();
 const { expect } = chai;
@@ -15,7 +15,7 @@ chai.use(chaiHttp);
 describe('/GET Users', () => {
   it('It should return status of 200', (done) => {
     chai.request(server)
-      .get('/v1/user')
+      .get('/v1/auth/users')
       .end((err, res) => {
         res.should.have.a.status(200);
         done();
@@ -24,7 +24,7 @@ describe('/GET Users', () => {
 
   it('It should be an object(array of object prototype)', (done) => {
     chai.request(server)
-      .get('/v1/user')
+      .get('/v1/auth/users')
       .end((err, res) => {
         res.should.be.a('object');
         done();
@@ -33,7 +33,7 @@ describe('/GET Users', () => {
 
   it('It should get all users', (done) => {
     chai.request(server)
-      .get('/v1/user')
+      .get('/v1/auth/users')
       .end((err, res) => {
         expect(res.body).to.have.lengthOf(4);
         done();
@@ -42,10 +42,7 @@ describe('/GET Users', () => {
 });
 
 describe('/POST User', () => {
-  beforeEach(() => {
-    testDelete('precioustosin@hotmail.com');
-  });
-  it('it should return status of 200 ok', (done) => {
+  it('it should create new user return status of 200 ok', (done) => {
     const user = {
       id: '',
       firstName: 'Tosin',
@@ -57,30 +54,10 @@ describe('/POST User', () => {
       status: 'inactive',
     };
     chai.request(server)
-      .post('/v1/user')
+      .post('/v1/auth/signup')
       .send(user)
       .end((err, res) => {
         res.should.have.a.status(200);
-        done();
-      });
-  });
-
-  it('it should create new user', (done) => {
-    const user = {
-      id: '',
-      firstName: 'Tosin',
-      lastName: 'Akinbobola',
-      email: 'precioustosin@hotmail.com',
-      password: 'precious',
-      type: 'client',
-      isAdmin: false,
-      status: 'inactive',
-    };
-    chai.request(server)
-      .post('/v1/user')
-      .send(user)
-      .end((err, res) => {
-        expect(res.body).to.have.lengthOf(5);
         done();
       });
   });
@@ -99,7 +76,7 @@ describe('POST/ User', () => {
       status: 'inactive',
     };
     chai.request(server)
-      .post('/v1/user')
+      .post('/v1/auth/signup')
       .send(user)
       .end((err, res) => {
         expect(res.body.error).to.be.equal('User exists');
@@ -115,7 +92,7 @@ describe('POST/ User', () => {
       password: 'jamesdonovan',
     };
     chai.request(server)
-      .post('/v1/user/login')
+      .post('/v1/auth/signin')
       .send(user)
       .end((err, res) => {
         res.should.have.a.status(200);
