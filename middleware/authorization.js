ff9fafed20bb6775473e30afd7e6e1c3b48213af
precioustosin <precioustosin@hotmail.com>
2019-04-08 11:15:56 +0100

    worked on authorization middleware functions for user and admin

---
+++ middleware/authorization.js
@@ -0,0 +1,65 @@
+const { verifyToken } = require('../controllers/jwt-token');
+
+function extractToken(req) {
+  const token = req.headers['x-access-token'] || req.headers.authorization;
+  if (token === undefined) {
+    return false;
+  }
+
+  if (token.startsWith('Bearer ')) {
+    // Remove Bearer from string
+    return token.slice(7, token.length);
+  }
+  return false;
+}
+
+function isUser(req, res, next) {
+  const token = extractToken(req);
+
+  if (token !== false) {
+    verifyToken(token)
+      .then((response) => {
+        req.decoded = response;
+        next();
+      })
+      .catch(error => res.status(401).json({
+        status: 401,
+        error: error.message,
+      }));
+  } else {
+    return res.status(401).json({
+      status: 401,
+      message: 'Auth token is not supplied',
+    });
+  }
+  return 'token error';
+}
+
+function isAdmin(req, res, next) {
+  const token = extractToken(req);
+
+  if (token !== false) {
+    verifyToken(token)
+      .then((response) => {
+        if (response.isAdmin === true) {
+          next();
+        }
+        return res.status(401).json({
+          status: 401,
+          error: 'User is not an admin',
+        });
+      })
+      .catch(error => res.status(401).json({
+        status: 401,
+        error: error.message,
+      }));
+  } else {
+    return res.status(401).json({
+      status: 401,
+      error: 'Auth token is not supplied',
+    });
+  }
+  return 'token error';
+}
+
+module.exports = { isUser, isAdmin };
