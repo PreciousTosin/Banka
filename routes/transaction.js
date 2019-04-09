3c2f36eb8fee64b7a004500948c305e52109ce57
precioustosin <precioustosin@hotmail.com>
2019-04-09 17:22:18 +0100

    created route for debit transaction

--- routes/transaction.js
+++ routes/transaction.js
@@ -36,4 +36,25 @@ router.post('/:accountNumber/credit', isStaff, async (req, res) => {
   }
 });
 
+router.post('/:accountNumber/debit', isStaff, async (req, res) => {
+  try {
+    const creditPayload = {
+      id: '',
+      createdOn: '',
+      type: req.body.type,
+      accountNumber: Number(req.params.accountNumber),
+      cashier: Number(req.body.cashier),
+      amount: Number(req.body.amount),
+      oldBalance: 0,
+      newBalance: 0,
+    };
+    const newTransaction = await transaction.createTransaction(creditPayload);
+    const response = Object.assign({}, { status: 200, data: newTransaction });
+    res.status(200).json(response);
+  } catch (e) {
+    const errResponse = Object.assign({}, { status: 400, error: e });
+    res.status(400).json(errResponse);
+  }
+});
+
 module.exports = router;
