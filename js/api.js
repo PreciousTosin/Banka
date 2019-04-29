3b793f529b8fc177b8e78b341d4f41fb841e7ea7
precioustosin <precioustosin@hotmail.com>
2019-04-29 04:22:41 +0100

    Commit to fix error management bugs
    bug(controllers/user) fix cors
    - add error management to login
    
    [Delivers #165665006]

--- js/api.js
+++ js/api.js
@@ -1,9 +1,14 @@
 const fetchApi = (method, url, body = {},  headers = {}) => {
+  console.log('FETCH BODY: ', body);
   return fetch(url, {
     headers: { ...headers },
     method: method,
     body: JSON.stringify(body)
   })
+};
+
+const getFetchApi = (url) => {
+  return fetch(url);
 };
 
 const setToken = (token) => `Bearer ${token}`;
