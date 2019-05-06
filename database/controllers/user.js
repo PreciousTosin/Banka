c8241c194809cb8dcb73172fe9ee9239e7b3d43d
precioustosin <precioustosin@hotmail.com>
2019-05-06 08:51:51 +0100

    Commit to add status to user entity
    bug(api/user) update user model
    - add status field to user entity
    - modify controller function and models
    
    [Delivers #165665006]

--- database/controllers/user.js
+++ database/controllers/user.js
@@ -16,6 +16,7 @@ const generateUserPrint = (userPayload, admin) => ({
   password: userPayload.password,
   type: userPayload.type === 'staff' ? userPayload.type : 'client',
   isAdmin: admin === true ? admin : false,
+  status: 'active',
 });
 
 const tokenizeUser = userWithoutToken => new Promise((resolve, reject) => tokenUtility
@@ -122,6 +123,13 @@ class UserController {
       if (userData === undefined || userData.length === 0) {
         return res.status(404).json(Object.assign({}, { status: 404, error: 'User does not exist' }));
       }
+
+      // check if account is not suspended
+      if (userData[0].status === 'inactive') {
+        const errorResponse = Object.assign({}, { status: 400, error: 'Your account has been suspended.' });
+        return res.status(400).json(errorResponse);
+      }
+
       const isValidUser = await asyncComparePassword(userPayload.password, userData[0].password);
       if (isValidUser === true) {
         // generate user data to tokenize
