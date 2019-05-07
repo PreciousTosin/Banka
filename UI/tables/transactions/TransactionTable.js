d7c76ff6e658688cd132c6bc8dfcb911aba0336b
precioustosin <precioustosin@hotmail.com>
2019-05-07 03:16:43 +0100

    Commit to show live transaction data
    feature(UI/*) show all user transactions
    - added code to fetch transaction data and initialize table with fetched data
    
    [Delivers #165665006]

--- UI/tables/transactions/TransactionTable.js
+++ UI/tables/transactions/TransactionTable.js
@@ -1,6 +1,6 @@
-function initTransactionTable() {
+function initTransactionTable(data) {
   console.log('SETTING UP TRANSACTION TABLE');
-     const model = new TransactionTableModel(),
+     const model = new TransactionTableModel(data),
          view = new TransactionTableView(model),
          controller = new TransactionTableController(model, view);
   return controller;
--- UI/tables/transactions/TransactionTableModel.js
+++ UI/tables/transactions/TransactionTableModel.js
@@ -2,30 +2,10 @@ class TransactionTableModel extends TableModel {
   static dateTime(offset) {
     return new Date(Date.now() + offset);
   }
-  constructor() {
+  constructor(data) {
     super();
     this.tableHeader = [ 'id', 'createdon', 'type', 'accountnumber', 'cashier', 'amount', 'oldbalance', 'newbalance' ];
-    this.tableData = [
-      {
-        id: 612879,
-        createdon: TransactionTableModel.dateTime(0),
-        type: 'credit',
-        accountnumber: 281640892,
-        cashier: 36956,
-        amount: '150',
-        oldbalance: '350',
-        newbalance: '500',
-      }, {
-        id: 706845,
-        createdon: TransactionTableModel.dateTime(12 * 60 * 60 * 1000),
-        type: 'credit',
-        accountnumber: 280872904,
-        cashier: 36956655,
-        amount: '900',
-        oldbalance: '0',
-        newbalance: '900',
-      },
-    ];
+    this.tableData = data.length === 0 ? [] : data;
     this.tableType = 'transactions';
     this.loadTableEvent = new Event(this);
     this.viewTransactionEvent = new Event(this);
